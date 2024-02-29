a9a1274b27882e6dcb1100b5b8996cfe31020f9a
eminb61 <eminburak_onat@berkeley.edu>
2024-02-29 13:11:00 -0800

    new job.sh

--- job.sh
+++ job.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=rluam
 #SBATCH --account=fc_rluam
-#SBATCH --partition=savio2_gpu
+#SBATCH --partition=savio3_gpu
+#SBATCH --qos=gtx2080_gpu3_normal
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=2
-#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --time=02:00:00
 ## Command(s) to run:
 module load python/3.11
 
